301be122efa77d48eb24a54515519a7d7454ca1f
yumi <m15198806252@163.com>
2020-05-27 22:50:24 +0800

    登陆注册表单

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -14,22 +14,31 @@
         status-icon
         :rules="rules"
         ref="ruleForm"
-        label-width="100px"
-        class="demo-ruleForm"
+        size='medium'
       >
-        <el-form-item label="密码" prop="pass">
-          <el-input type="password" v-model="ruleForm.pass" autocomplete="off"></el-input>
+        <el-form-item  prop='userName'>
+            <label>邮箱</label>
+            <el-input type="text" v-model="ruleForm.userName" autocomplete="off"></el-input>
         </el-form-item>
-        <el-form-item label="确认密码" prop="checkPass">
-          <el-input type="password" v-model="ruleForm.checkPass" autocomplete="off"></el-input>
+        <el-form-item prop="Password" >
+            <label>密码</label>
+            <el-input type="password" v-model="ruleForm.Password" autocomplete="off"></el-input>
         </el-form-item>
-        <el-form-item label="年龄" prop="age">
-          <el-input v-model.number="ruleForm.age"></el-input>
+        <!-- 注册表单 -->
+         <el-form-item prop="checkPass"  v-if='this.model=="register"'>
+            <label>重复密码</label>
+            <el-input type="password" v-model="ruleForm.checkPass" autocomplete="off"></el-input>
+        </el-form-item>
+      <el-form-item  prop="Code" >
+            <label>验证码</label>
+            <el-row :gutter="11">
+              <el-col :span="15"><el-input v-model.number="ruleForm.Code"></el-input></el-col>
+              <el-col :span="6"><el-button type="success">获取验证码</el-button></el-col>
+            </el-row>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
-          <el-button @click="resetForm('ruleForm')">重置</el-button>
-        </el-form-item>
+                <el-button type="primary" @click="onLogin">{{subutton}}</el-button>
+       </el-form-item>
       </el-form>
     </div>
   </div>
@@ -39,9 +48,9 @@ export default {
   name: "login",
   data() {
     //   登陆表单验证
-      var checkAge = (rule, value, callback) => {
+      var checkCode = (rule, value, callback) => {
         if (!value) {
-          return callback(new Error('年龄不能为空'));
+          return callback(new Error('验证码不能为空'));
         }
         setTimeout(() => {
           if (!Number.isInteger(value)) {
@@ -55,10 +64,25 @@ export default {
           }
         }, 1000);
       };
-      var validatePass = (rule, value, callback) => {
+      var validateUser = (rule, value, callback) => {
+    //   var reg=/^[A-Za-z0-9\u4e00-\u9fa5]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/;
+        if (value === '') {
+          callback(new Error('请输入邮箱'));
+         }//else if(reg.text(value)){
+        //     callback(new Error('邮箱格式不对'));
+        // } 
+        else {
+          if (this.ruleForm.checkPass !== '') {
+            this.$refs.ruleForm.validateField('checkPass');
+          }
+          callback();
+        }
+      };
+        var validatePass = (rule, value, callback) => {
         if (value === '') {
           callback(new Error('请输入密码'));
-        } else {
+         }
+        else {
           if (this.ruleForm.checkPass !== '') {
             this.$refs.ruleForm.validateField('checkPass');
           }
@@ -67,7 +91,7 @@ export default {
       };
       var validatePass2 = (rule, value, callback) => {
         if (value === '') {
-          callback(new Error('请再次输入密码'));
+          callback(new Error('请确认密码'));
         } else if (value !== this.ruleForm.pass) {
           callback(new Error('两次输入密码不一致!'));
         } else {
@@ -76,25 +100,31 @@ export default {
       };
     return {
       loginmenu: [
-        { txt: "登录", isActive: true },
-        { txt: "注册", isActive: false }
+        { txt: "登录", isActive: true,type:'login',Subtn:'立即登录' },
+        { txt: "注册", isActive: false,type:'register',Subtn:'立即注册' }
       ],
+      model:'login',
+      subutton:'立即登录',
       isActive: true,//登录注册按钮选中切换
     //   登陆表单验证
      ruleForm: {
-          pass: '',
+          userName: '',
+          Password:'',
           checkPass: '',
-          age: ''
+          Code: ''
         },
         rules: {
-          pass: [
+          userName: [
+            { validator: validateUser, trigger: 'blur' }
+          ],
+          Password:[
             { validator: validatePass, trigger: 'blur' }
           ],
           checkPass: [
             { validator: validatePass2, trigger: 'blur' }
           ],
-          age: [
-            { validator: checkAge, trigger: 'blur' }
+          Code: [
+            { validator: checkCode, trigger: 'blur' }
           ]
         }
      
@@ -109,12 +139,15 @@ export default {
         elem.isActive = false;
       });
       e.isActive = true;
+      this.model=e.type;
+      this.subutton=e.Subtn;
+      console.log(this.subutton,123)
     },
-    //点击提交表单
-    submitForm(formName) {
+    //点击登录
+    onLogin(formName) {
         this.$refs[formName].validate((valid) => {
           if (valid) {
-            alert('submit!');
+            alert('submit!',8888);
           } else {
             console.log('error submit!!');
             return false;
@@ -151,5 +184,18 @@ export default {
       background-color: rgba(0, 0, 0, 0.1);
     }
   }
+  label{
+    display:block;
+    color:#fff;
+    font-size: 13px;
+  }
+  .el-form-item{
+      margin-bottom: 10px;
+  }
+  .el-button--primary{
+      width: 100%;
+      margin: 4% 0;
+  }
+
 }
 </style>
